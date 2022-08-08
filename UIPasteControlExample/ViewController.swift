14c4c34e692ae066b77b69ab5e531e722bd8eb7a
Kyle Lanchman <k.lanchman@gmail.com>
2022-08-08 15:38:02 -0400

    Ignore returned Progress
    Doesn't seem like they need to be retained

--- UIPasteControlExample/ViewController.swift
+++ UIPasteControlExample/ViewController.swift
@@ -10,7 +10,6 @@ import UniformTypeIdentifiers
 
 class ViewController: UIViewController {
     private let pasteControl = UIPasteControl()
-    private var progresses = [Progress]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,17 +35,13 @@ extension ViewController {
     override func paste(itemProviders: [NSItemProvider]) {
         for provider in itemProviders {
             if provider.hasItemConformingToTypeIdentifier(UTType.text.identifier) {
-                let progress = provider.loadObject(ofClass: String.self) { str, error in
+                _ = provider.loadObject(ofClass: String.self) { str, error in
                     print(str as Any, error as Any)
                 }
-
-                progresses.append(progress)
             } else if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-                let progress = provider.loadObject(ofClass: UIImage.self) { img, error in
+                _ = provider.loadObject(ofClass: UIImage.self) { img, error in
                     print(img as Any, error as Any)
                 }
-
-                progresses.append(progress)
             }
         }
     }
